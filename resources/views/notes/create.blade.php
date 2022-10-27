d1f6794c08d153d2c04b8d6dc5d6b0bf288f20f6
India.h <indihth@yahoo.co.uk>
2022-10-27 16:04:59 +0100

    Created form for New Note

---
+++ resources/views/notes/create.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Notes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                <form action="{{ route('notes.store') }}" method="post">
+                    {{-- @csrf is used to generate a hidden field with 
+                        a csrf token to validate the request is being made 
+                        by an authorised user. Prevent outside attacks--}}
+                    @csrf
+                    <x-text-input type="text" name="title" placeholder="Title" class="w-full" autocomplete="off"></x-text-input>
+                    <x-textarea name="text" rows="10" placeholder="Start typing here..." class="w-full mt-6"></x-textarea>
+                    <x-primary-button class="mt-6">Save</x-primary-button>
+                </form>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
