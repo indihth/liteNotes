6f4eea385b4e363d0b3ff6eb30cf22317542a680
India.h <indihth@yahoo.co.uk>
2022-10-27 16:57:12 +0100

    Add empty guarded array to allow add note to db

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    //////////////////////////
+    // Get around mass assignment block when adding note
+    //////////////////////////
+
+    // Fields that can be mass assiged with:
+    //
+    // protected $fillable = []
+
+    // Fields to prevent against mass assignment.
+    // Using empty array allows all fields to be mass assigned.
+    protected $guarded = [];
 }
