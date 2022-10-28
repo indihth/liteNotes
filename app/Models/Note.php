8d73186e4ed882758e4a8649ed5c84f406b63b8e
India.h <indihth@yahoo.co.uk>
2022-10-28 10:44:12 +0100

    Changed default url to use UUID

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -12,12 +12,23 @@ class Note extends Model
     //////////////////////////
     // Get around mass assignment block when adding note
     //////////////////////////
-
+    
     // Fields that can be mass assiged with:
     //
     // protected $fillable = []
-
+    
     // Fields to prevent against mass assignment.
     // Using empty array allows all fields to be mass assigned.
     protected $guarded = [];
+    
+
+    //////////////////////////
+    // Because I've passed Note directly into show() in NoteController
+    // it uses note->id by default, 
+    // this changes the default to use note->uuid
+    //////////////////////////
+    public function getRouteKeyName()
+    {
+        return 'uuid';
+    }
 }
