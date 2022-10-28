452fff57e91cca2c3cd70bc97b5997e5ecc6583b
India.h <indihth@yahoo.co.uk>
2022-10-28 11:26:11 +0100

    Used wrong note view. Changed .edit to .update

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -10,10 +10,10 @@
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
 
 
-                <form action="{{ route('notes.edit', $note) }}" method="post">
-                    {{-- Laravel resource controller edit uses method PUT/PATCH but HTML can't use either so we 
+                <form action="{{ route('notes.update', $note) }}" method="post">
+                    {{-- Laravel resource controller update uses method PUT/PATCH but HTML can't use either so we 
                     need to spoof it by adding the @method('put') in the form --}}
-                    @method('put');
+                    @method('put')
 
                     {{-- @csrf is used to generate a hidden field with a csrf token to validate the request is being made 
                         by an authorised user. Prevent outside attacks--}}
